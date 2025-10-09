8e9fa0b0a832416cefe5d8976110edda9e4e7523
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-10-09 22:45:25 +0200

    Refactor: Updated RenderLoginForm to add the /guestbook/api/login path

--- apps/guestbook/Helpers/HtmlHelper.cs
+++ apps/guestbook/Helpers/HtmlHelper.cs
@@ -33,7 +33,7 @@ public static class HtmlHelper
         return @"
             <div id='admin-section'>
                 <h3>Admin Login</h3>
-                <form hx-post='/api/login' hx-target='#admin-section' hx-swap='outerHTML'>
+                <form hx-post='/guestbook/api/login' hx-target='#admin-section' hx-swap='outerHTML'>
                     <input type='text' name='username' placeholder='Username' required /><br>
                     <input type='password' name='password' placeholder='Password' required /><br>
                     <button type='submit'>Login</button>
